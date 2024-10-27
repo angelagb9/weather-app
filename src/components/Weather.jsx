9a33f428080dfbd350b08a943ef08222f6f18fe3
angelagb9 <angelagar910@gmail.com>
2024-10-26 19:53:47 -0700

    add functionality

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search-icon.png'
 import clear from '../assets/clear.png'
@@ -9,38 +9,103 @@ import rain from '../assets/rain.png'
 import snow from '../assets/snow.png'
 import wind from '../assets/wind.png'
 
+const icons = {
+    "01d": clear,
+    "01n": clear,
+    "02d": cloud,
+    "02n": cloud,
+    "03d": cloud,
+    "03n": cloud,
+    "04d": drizzle,
+    "04n": drizzle,
+    "09d": rain,
+    "09n": rain,
+    "10d": rain,
+    "10n": rain,
+    "13d": snow,
+    "13n": snow,
+}
+
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const APP_ID = process.env.REACT_APP_API_KEY;
+
 const Weather = () => {
+
+    const [weatherData, setWeatherData] = useState(null)
+    const [city, setCity] = useState("");
+
+    const search = async (cityName) => {
+        if(cityName === ''){
+            alert("Please select a city")
+            return
+        }
+        try {
+            const url = `${API_URL}?q=${cityName}&units=metric&appid=${APP_ID}`
+            const response = await fetch(url)
+            const data = await response.json()
+
+            if(!response.ok){
+                alert(data.message || "Failed to fetch weather data");
+                return  
+            }
+
+            const icon = icons[data.weather[0].icon] || clear
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name, 
+                icon: icon
+            })
+        }
+        catch (err) {
+            setWeatherData(null)
+            console.error("Error fetching weather data:", err);
+        }
+    }
+
+    useEffect(()=>{
+        search('Mexicali')
+    },[])
+
     return (
         <div className='weather'>
             <div className='search-bar'>
-                <input type='text' placeholder='Search'/>
-                <img src={search_icon} width="30" height="30"/>
+                <input 
+                    type='text' 
+                    placeholder='Search' 
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <img src={search_icon} width="30" height="30" alt='' onClick={()=>search(city)}/>
             </div>
-            <div class='weather-tempeture'>
+            {weatherData?<>
+                <div className='weather-tempeture'>
                 <div className='col'>
-                    <h2 className='tempeture'>16º</h2>
+                    <h2 className='tempeture'>{weatherData.temperature}</h2>
                 </div>
                 <div className='col'>
-                    <img src={clear} className='weather-icon'/>
+                    <img src={weatherData.icon} className='weather-icon' alt=''/>
                 </div>
             </div>
-            <h3 className='location'>Mexico</h3>
+            <h3 className='location'>{weatherData.location}</h3>
             <div className='weather-data'>
                 <div className='col'>
-                    <img src={humidity}/>
+                    <img src={humidity} alt=''/>
                     <div>
-                        <p>91%</p>
+                        <p>{weatherData.humidity}%</p>
                         <span>Humidity</span>
                     </div>
                 </div>
                 <div className='col'>
-                    <img src={wind}/>
+                    <img src={wind} alt=''/>
                     <div>
-                        <p>34 km/h</p>
+                        <p>{weatherData.windSpeed} km/h</p>
                         <span>Wind Speed</span>
                     </div>
                 </div>
             </div>
+            </>:<></>}
         </div>
     )
 }
